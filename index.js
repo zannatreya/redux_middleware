f9e412a0f553016f10232a33b79443fbfee9c9f1
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-21 16:37:00 +0600

    apply middleware

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const { createStore, combineReducers } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
 
 //product constants
 
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-//cart constants
-
-const GET_CART_ITEMS = "GET_CART_ITEMS";
-const ADD_CART_ITEMS = "ADD_CART_ITEMS";
 
 //product state
 
@@ -16,12 +13,7 @@ const initialProductState={
     products:["salt","sugar"],
     numberOfProducts: 2,
 };
-//cart state
 
-const initialCartState={
-    cart:["sugar"],
-    numberOfProducts: 1,
-};
 
 //product action
 
@@ -33,19 +25,6 @@ const getProductAction =()=>{
 const addProductAction =(product)=>{
     return{
         type: ADD_PRODUCTS,
-        payload: product,
-    };
-};
-//cart action
-
-const getCartAction =()=>{
-    return{
-        type: GET_CART_ITEMS,
-    };
-};
-const addCartAction =(product)=>{
-    return{
-        type: ADD_CART_ITEMS,
         payload: product,
     };
 };
@@ -69,35 +48,10 @@ switch (action.type) {
     return state;
     }
 }
-//cart reducer
-
-const cartReducer =(state=initialCartState,action)=>{
-switch (action.type) {
-    case GET_CART_ITEMS:
-        return{
-            ...state,
-        };
-    case ADD_CART_ITEMS:
-        return{
-            cart: [...state.cart,action.payload],
-            numberOfProducts: state.numberOfProducts + 1,
-        };
-        
-
-    default:
-    return state;
-    }
-};
-
-const rootReducer = combineReducers({
-productR: productsReducer,
-cartR: cartReducer,
-});
 
 // store
 
-// const store = createStore(productsReducer);
-const store = createStore(rootReducer);
+const store = createStore(productsReducer,applyMiddleware(logger));
 
 store.subscribe(()=>{
     console.log(store.getState());
@@ -106,5 +60,3 @@ store.subscribe(()=>{
 store.dispatch(getProductAction());
 store.dispatch(addProductAction("ata"));
 store.dispatch(addProductAction("moida"));
-store.dispatch(getCartAction());
-store.dispatch(addCartAction("suji"));
